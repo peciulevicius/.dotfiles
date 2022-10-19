54d7d1a8acf3a1fa002a088057a2ea02288d8496
Džiugas Pečiulevičius <43075730+peciulevicius@users.noreply.github.com>
2022-10-19 19:18:54 +0300

    add more keymaps

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -18,7 +18,7 @@ vim.g.maplocalleader = " "
 --   term_mode = "t",
 --   command_mode = "c",
 
--- Normal --
+---------- NORMAL ----------
 -- Better window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
@@ -36,3 +36,30 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+---------- INSERT ----------
+-- Press jk fast to exit insert mode 
+keymap("i", "jk", "<ESC>", opts)
+
+---------- VISUAL ----------
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Keeps the same item inside a register when pasting over some text 
+-- that would be yanked and stored inside a register by default
+keymap("v", "p", '"_dP', opts)
+
+---------- VISUAL BLOCK ----------
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+---------- TERMINAL ----------
+-- Better terminal navigation
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
