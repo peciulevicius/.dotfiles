4c2934eea7b8afa030609498ed576520bc1b89d8
Džiugas Pečiulevičius <43075730+peciulevicius@users.noreply.github.com>
2022-10-22 19:20:02 +0300

    add cmp (code completion) support for nvim

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -42,12 +42,24 @@ packer.init {
 -- Install your plugins here
 return packer.startup(function(use)
   -- My plugins here
-  use "wbthomason/packer.nvim"  -- Have packer manage itself
-  use "nvim-lua/popup.nvim"     -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim"   -- Useful lua functions used by lots of plugins
+  use "wbthomason/packer.nvim"        -- Have packer manage itself
+  use "nvim-lua/popup.nvim"           -- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim"         -- Useful lua functions used by lots of plugins
   use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'} -- Displays markdown preview (:MarkdownPreview)
-  use "folke/tokyonight.nvim"
 
+  -- Colorschemes
+  use "folke/tokyonight.nvim"         -- tokyonight colorscheme
+
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp"              -- The completion plugin
+  use "hrsh7th/cmp-buffer"            -- buffer completions
+  use "hrsh7th/cmp-path"              -- path completions
+  use "hrsh7th/cmp-cmdline"           -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip"      -- snippet completions
+
+  -- snippets
+  use "L3MON4D3/LuaSnip"              --snippet engine
+  use "rafamadriz/friendly-snippets"  -- a bunch of snippets to use
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
