ce1ac83883df9c81dfddf031b87c0320b8965201
Džiugas Pečiulevičius <43075730+peciulevicius@users.noreply.github.com>
2022-10-23 19:06:21 +0300

    add autopairs plugin to automatically add , , etc.

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -46,6 +46,7 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim"                 -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim"               -- Useful lua functions used by lots of plugins
   use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'} -- Displays markdown preview (:MarkdownPreview)
+  use "windwp/nvim-autopairs"               -- Autopairs, integrates with both cmp and treesitter
 
   -- Colorschemes
   use "folke/tokyonight.nvim"               -- tokyonight colorscheme
