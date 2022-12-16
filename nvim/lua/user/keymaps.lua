11a8292e5a97b3466ad86926b4feaa7489c8a2ca
Džiugas Pečiulevičius <43075730+peciulevicius@users.noreply.github.com>
2022-12-16 20:22:23 +0200

    add latest config before nvim refactor

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -28,10 +28,10 @@ keymap("n", "<C-l>", "<C-w>l", opts)
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<S-Up>", ":resize +2<CR>", opts)
+keymap("n", "<S-Down>", ":resize -2<CR>", opts)
+keymap("n", "<S-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<S-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
@@ -65,10 +65,10 @@ keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
-
 ---------- TELESCOPE ----------
 -- fuzzy finder
+keymap("n", "<C-S-f>", "<cmd>Telescope live_grep<cr>", opts)      -- search keywords
 keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)   -- search all files and show preview
 -- search all files and hide preview
 -- keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
-keymap("n", "<C-S-f>", "<cmd>Telescope live_grep<cr>", opts)      -- search keywords
+
